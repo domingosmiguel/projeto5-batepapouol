12e6e31d664c68509e68bbbf5454199938437ca8
migueldsp <migueldsp@hotmail.com>
2022-08-27 21:58:36 -0300

    improved login logic + pointer over buttons

--- src/index.js
+++ src/index.js
@@ -4,39 +4,38 @@ const newMessage = (messagesContainer, message) => {
      if (message.type === "status") {
           messagesContainer.innerHTML += `${pagesData[message.type]} 
                                              <p style="color: rgb(170,170,170);">
-                                                  (${message.time})&#160 
+                                                  (${message.time})
                                              </p>
                                              <p style="font-weight: 700;">${message.from}</p>
-                                             &#160${message.text}
+                                             <p>${message.text}</p>
                                         </li> `;
      } else if (
           message.type === "private_message" &&
-          (message.to === (userMessage.name || "Todos") || message.from === userMessage.name)
+          (message.to === userMessage.name ||
+               message.to === "Todos" ||
+               message.from === userMessage.name)
      ) {
           messagesContainer.innerHTML += `${pagesData[message.type]} 
                                              <p style="color: rgb(170,170,170);">
-                                                  (${message.time})&#160 
+                                                  (${message.time})
                                              </p>
                                              <p style="font-weight: 700;">${message.from}</p>
-                                             &#160reservadamente&#160para&#160
+                                             <p> reservadamente para </p>
                                              <p style="font-weight: 700;">${message.to}:</p>
-                                             &#160${message.text}
+                                             <p>${message.text}</p>
                                         </li> `;
      } else if (message.type === "message") {
           messagesContainer.innerHTML += `${pagesData[message.type]} 
                                              <p style="color: rgb(170,170,170);">
-                                                  (${message.time})&#160 
+                                                  (${message.time})
                                              </p>
                                              <p style="font-weight: 700;">${message.from}</p>
-                                             &#160para&#160
+                                             <p> para </p>
                                              <p style="font-weight: 700;">${message.to}:</p>
-                                             &#160${message.text}
+                                             <p> ${message.text}</p>
                                         </li> `;
      }
      document.querySelector("main").scrollTo(0, document.querySelector("main").scrollHeight);
-};
-const serverDidNotReceiveMessage = () => {
-     console.log("server did not receive message");
 };
 const sendNewUserMessage = (userMessageText) => {
      userMessage.text = userMessageText;
@@ -45,7 +44,7 @@ const sendNewUserMessage = (userMessageText) => {
           userMessage
      );
      sendMessageToServer.then(getMessagesFromServer);
-     sendMessageToServer.catch(serverDidNotReceiveMessage);
+     sendMessageToServer.catch(connectionError);
 };
 const messageSettingsSelector = (target, targetParent) => {
      const allChildren = targetParent.querySelectorAll("li");
@@ -62,15 +61,16 @@ const messageFilter = (clickedButton) => {
      const inputHTML = document.querySelector("footer > div > input");
      if (inputHTML.value && clickedButton === "Enter") {
           sendNewUserMessage(inputHTML.value);
+     } else if (clickedButton === "Escape") {
+          inputHTML.blur();
      }
-     inputHTML.blur();
      inputHTML.value = "";
 };
 const eventListenersSetup = () => {
      document.querySelector("#sendButton").addEventListener("click", () => {
           messageFilter("Enter");
      });
-     document.addEventListener("keydown", (e) => {
+     document.querySelector("div.messagesContainer").addEventListener("keydown", (e) => {
           if (e.key === "Enter" || e.key === "Escape") {
                messageFilter(e.key);
           }
@@ -117,11 +117,10 @@ const messagePrivacyAndDestination = (info, type) => {
           case "Reservadamente":
                document.querySelector(
                     "div.msg-infos"
-               ).innerHTML = `Enviando para ${userMessage.to} (Reservadamente)`;
+               ).innerHTML = `Enviando para ${userMessage.to} (reservadamente)`;
                userMessage.type = "private_message";
                break;
      }
-     console.log(userMessage);
 };
 let LastRenderedMessage = false;
 
@@ -132,36 +131,22 @@ const renderServerMessages = (serverPromise) => {
           serverMessages.forEach((serverMessage) => {
                newMessage(messagesContainer, serverMessage);
           });
-          LastRenderedMessage = serverMessages[serverMessages.length - 1];
      } else if (serverMessages[serverMessages.length - 1].time !== LastRenderedMessage.time) {
-          LastRenderedMessage = serverMessages[serverMessages.length - 1];
           for (let i = serverMessages.length - 2; i > 0; i--) {
-               if (serverMessages[i].time !== LastRenderedMessage.time) {
+               if (serverMessages[i].time === LastRenderedMessage.time) {
                     for (i++; i < serverMessages.length; i++) {
                          newMessage(messagesContainer, serverMessages[i]);
-                         if (serverMessages[i].type === "status") {
-                              switch (serverMessages[i].text) {
-                                   case "entra na sala...":
-                                        newUserOnline(serverMessages[i].from);
-                                        break;
-                                   case "sai da sala...":
-                                        newUserOffline(serverMessages[i].from);
-                                        break;
-                              }
-                         }
                     }
                     break;
                }
           }
      }
-};
-const didNotLoadServerMessages = () => {
-     console.log("Did not load server messages");
+     LastRenderedMessage = serverMessages[serverMessages.length - 1];
 };
 const getMessagesFromServer = () => {
      const promiseFromServer = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
      promiseFromServer.then(renderServerMessages);
-     promiseFromServer.catch(didNotLoadServerMessages);
+     promiseFromServer.catch(connectionError);
 };
 const eventListenersForUsersCards = () => {
      document.querySelectorAll("ul.users li").forEach((item) => {
@@ -173,7 +158,7 @@ const eventListenersForUsersCards = () => {
           });
      });
 };
-const newUserOnline = (userName) => {
+const newOnlineUser = (userName) => {
      const usersContainer = document.querySelector("ul.users");
      usersContainer.innerHTML += `<li data-identifier="participant">
                                         <div>
@@ -184,82 +169,149 @@ const newUserOnline = (userName) => {
                                    </li>`;
      eventListenersForUsersCards();
 };
-const newUserOffline = (userName) => {
-     const usersCards = document.querySelectorAll("ul.users > li:not(:first-child)");
+const newOfflineUser = (userName) => {
+     const targetParent = document.querySelector("ul.users");
+     const usersCards = targetParent.querySelectorAll("li:not(:first-child)");
      Object.values(usersCards).some((card) => {
           const userFromCard = card.querySelector("div.userName").innerHTML;
           if (userName == userFromCard) {
                card.remove();
                if (card.classList.value === "check") {
-                    const targetParent = document.querySelector("ul.users");
-                    const target = targetParent.querySelector("li:first-child");
-                    messageSettingsSelector(target, targetParent);
+                    const allTargets = targetParent.querySelectorAll("li");
+                    Object.values(allTargets).some((target) => {
+                         const userCard = target.querySelector("div.userName").innerHTML;
+                         if (userCard === userMessage.name) {
+                              messageSettingsSelector(target, targetParent);
+                              return true;
+                         }
+                         return false;
+                    });
                }
                return true;
           }
           return false;
      });
 };
-const renderUsersOnlineForTheFirstTime = (serverPromise) => {
-     const usersOnline = serverPromise.data;
-     usersOnline.forEach((user) => {
-          newUserOnline(user.name);
-     });
+let localListOfOnlineUsers = false;
+
+const renderOnlineUsers = (serverPromise) => {
+     const serverUsers = serverPromise.data;
+     if (!localListOfOnlineUsers) {
+          serverUsers.forEach((user) => {
+               newOnlineUser(user.name);
+          });
+     } else {
+          const offlineUsers = localListOfOnlineUsers.slice(0);
+          const onlineUsers = serverUsers.slice(0);
+          onlineUsers.forEach((onlineUser, i) => {
+               offlineUsers.some((offlineUser, j) => {
+                    if (onlineUser.name === offlineUser.name) {
+                         onlineUsers[i] = false;
+                         offlineUsers[j] = false;
+                         return true;
+                    }
+                    return false;
+               });
+          });
+          onlineUsers.forEach((onlineUser) => {
+               if (onlineUser) {
+                    newOnlineUser(onlineUser.name);
+               }
+          });
+          offlineUsers.forEach((offlineUser) => {
+               if (offlineUser) {
+                    newOfflineUser(offlineUser.name);
+               }
+          });
+     }
+     localListOfOnlineUsers = serverUsers;
 };
-const didNotLoadUsersOnline = () => {
-     console.log("Did not load users online");
-};
-const getUsersOnline = () => {
+const getOnlineUsers = () => {
      const promiseFromServer = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
-     promiseFromServer.then(renderUsersOnlineForTheFirstTime);
-     promiseFromServer.catch(didNotLoadUsersOnline);
+     promiseFromServer.then(renderOnlineUsers);
+     promiseFromServer.catch(connectionError);
 };
-const connectionError = () => {
-     document.querySelector("body").innerHTML = pagesData.loginPage;
-     document.querySelector("#errorMessage").innerHTML = "Lost server connection";
+const connectionError = (error) => {
      Object.values(ids).forEach((id) => {
           clearInterval(id);
+          console.log(id);
      });
-     localeStoredServerMessages.splice(0);
-     loginLogic();
+     localListOfOnlineUsers = false;
+     LastRenderedMessage = false;
+     loginPageLogic(error.config.url);
+};
+const keepConnection = () => {
+     const connection = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userMessage);
+     connection.catch(connectionError);
 };
 const ids = {};
 
 const loginSuccess = () => {
      document.querySelector("body").innerHTML = pagesData.mainPage;
+     document.querySelector("div.msg-infos").innerHTML = `Enviando para Todos`;
+     userMessage.to = "Todos";
+     userMessage.type = "message";
      eventListenersSetup();
      getMessagesFromServer();
-     getUsersOnline();
+     getOnlineUsers();
      ids.connection = setInterval(() => {
-          const connection = axios.post(
-               "https://mock-api.driven.com.br/api/v6/uol/status",
-               userMessage
-          );
-          connection.catch(connectionError);
+          keepConnection();
      }, 5000);
      ids.loadMessages = setInterval(() => {
           getMessagesFromServer();
      }, 3000);
+     ids.OnlineUsers = setInterval(() => {
+          getOnlineUsers();
+     }, 10000);
 };
-const loginError = (error) => {
-     document.querySelector("#errorMessage").innerHTML = error.message;
+const loginError = () => {
+     document.querySelector("#loginButton").classList.remove("hidden");
+     document.querySelector("#userName").classList.remove("hidden");
+     document.querySelector(".loader").classList.add("hidden");
+     document.querySelector(
+          "#errorMessage"
+     ).innerHTML = `Por favor, insira outro nome de usuário pois este já está em uso`;
 };
 const userMessage = {};
 
-const loginLogic = () => {
+const loginLogic = (clickedButton) => {
+     if (
+          document.querySelector("#userName").value &&
+          (clickedButton === "Enter" || clickedButton === "clicked")
+     ) {
+          document.querySelector("#loginButton").classList.add("hidden");
+          document.querySelector("#userName").classList.add("hidden");
+          document.querySelector(".loader").classList.remove("hidden");
+          userMessage.from = userMessage.name = document.querySelector("#userName").value;
+          const loginRequest = axios.post(
+               "https://mock-api.driven.com.br/api/v6/uol/participants",
+               userMessage
+          );
+          loginRequest.then(loginSuccess);
+          loginRequest.catch(loginError);
+     }
+};
+const loginPageListeners = () => {
      document.querySelector("#loginButton").addEventListener("click", () => {
-          if (document.querySelector("#userName").value) {
-               userMessage.name = userMessage.from = document.querySelector("#userName").value;
-               userMessage.to = "Todos";
-               userMessage.type = "message";
-               const loginRequest = axios.post(
-                    "https://mock-api.driven.com.br/api/v6/uol/participants",
-                    userMessage
-               );
-               loginRequest.then(loginSuccess);
-               loginRequest.catch(loginError);
-          }
+          loginLogic("clicked");
+     });
+     document.querySelector("main.loginPage").addEventListener("keypress", (e) => {
+          loginLogic(e.key);
      });
 };
-
-loginLogic();
+const loginPageLogic = (errorConfigURL) => {
+     document.querySelector("body").innerHTML = pagesData.loginPage;
+     loginPageListeners();
+     switch (errorConfigURL) {
+          case "https://mock-api.driven.com.br/api/v6/uol/status":
+               document.querySelector("#errorMessage").innerHTML = "Lost server connection";
+               break;
+          case "https://mock-api.driven.com.br/api/v6/uol/messages":
+               document.querySelector("#errorMessage").innerHTML = "Couldn't load new messages";
+               break;
+          case "https://mock-api.driven.com.br/api/v6/uol/participants":
+               document.querySelector("#errorMessage").innerHTML = "Couldn't load online users";
+               break;
+     }
+};
+loginPageLogic();
